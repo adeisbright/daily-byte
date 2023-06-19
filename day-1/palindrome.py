9ba4de9ada4ab7127007891c2e90e6c0d27dd547
Adeleke Bright <bigjaratech@gmail.com>
2023-06-19 18:15:17 +0100

    Add Helper functions for Some RegEx methods

--- day-1/palindrome.py
+++ day-1/palindrome.py
@@ -1,4 +1,6 @@
 from string_reversal import reverse_using_join
+import re 
+
 '''
 Given a string, return whether or not it forms a palindrome 
 ignoring case and non-alphabetical characters.
@@ -25,11 +27,14 @@ print(palindrome("A man , a plan , a canal; Panama"))
 def palindrome_second(word : str) -> bool :
     ''' Returns true if the word is a valid palindrome ''' 
     test_word = word.strip() 
-    special_chars = ("," , ";" , " ")
+    #special_chars = ("," , ";" , " ")
     valid_chars = "" 
 
+    pattern = re.compile(r"\W+")
     for char in test_word:
-        if char not in special_chars:
+
+        if not pattern.match(char):
+        #if char not in special_chars:
             valid_chars = valid_chars + char 
 
     worded = valid_chars.lower() 
@@ -45,8 +50,11 @@ def recursive_palindrome(word):
         The basic idea for recursion is that the function calls itself 
 
         A recursive function needs to know when it needs to end if you need 
-        it to end at some points.
+        it to end at some points (base case)
 
+        Find a way to break down the problem into sets of repeatable steps 
+        Then use the smallest break or the remainder after breaking to call 
+        itself (Each subproblem leads to another sub problem that follwos saem pattern)
     '''
     if len(word) == 0 or  len(word) == 1 :  return True 
 
@@ -58,4 +66,7 @@ def recursive_palindrome(word):
    
     return recursive_palindrome(mid)
 
-print(recursive_palindrome("aba"))
+#print(recursive_palindrome("aba"))
+
+
+
