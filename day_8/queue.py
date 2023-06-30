c09d7de8df26d854a5371faa30cf9b849f2f6d94
Adeleke Bright <bigjaratech@gmail.com>
2023-06-30 10:06:59 +0100

    Implement Queue using a Linked List

---
+++ day_8/queue.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self,value) -> None:
+        self.value = value 
+        self.next = None 
+
+
+class Queue :
+    def __init__(self) -> None:
+        self.front = None 
+        self.back = None 
+        self.size = 0 
+
+    def enqueue(self,value):
+        node = Node(value)
+        if self.front is None:
+            self.front = node 
+        else:
+            self.back.next = node 
+
+        self.back  = node  
+        self.size += 1
+
+    def dequeue(self):
+
+        node = self.front 
+        self.front = self.front.next
+        self.size -= 1
+        return node.value 
+
+    def peek(self):
+        if self.front is None:
+            return None 
+        return self.front.value  
+    
+    def length(self):
+        return self.size  
+    
+    def show(self):
+        current_front = self.front 
+
+        members = ''
+        while current_front :
+
+            members += "{value}->".format(value=current_front.value)
+
+            current_front = current_front.next 
+        print(members)
+
+
+if __name__ == "__main__":
+    q = Queue() 
+
+    top = q.peek() 
+    size = q.length() 
+    print("The value at the top is {} and queue size is {}".format(top , size))
+   
+    q.enqueue(10) 
+    q.enqueue(4)
+    q.enqueue(9)
+    q.enqueue(3)
+    top = q.peek() 
+    size = q.length() 
+    print("The value at the top is {} and queue size is {}".format(top , size))
+    q.show()
+    q.dequeue() 
+    q.show()
+    q.dequeue() 
+    q.show()
+    
+
+        
