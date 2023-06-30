77c30f50ab6054c7073278ac8a95f8c412cd45da
Adeleke Bright <bigjaratech@gmail.com>
2023-06-30 05:08:18 +0100

    Implemented Find and Delete Method for Linked List

--- day_7/linked_list.py
+++ day_7/linked_list.py
@@ -64,13 +64,59 @@ class LinkedList:
             head = head.next
         print(list_items)
 
+    def find(self , value):
+        ''' Returns true if a value exists in a linked list ''' 
+
+        if self.head is None : 
+            return False
+
+        head = self.head 
+        while head:
+            if head.value == value : 
+                return True
+            head = head.next 
+        
+        return False
+    
+    def delete(self , value):
+        ''' Removes a value from  a linked list if it exists 
+            To be able to delete an item from a linked list, we 
+            basically need to re-assign the previous node 
+            to replace the item to be deleted. 
+            To achieve this, we will use two pointers one that always 
+            points to the previous node, and one that points to the next node
+
+        '''
+        if self.head is None : 
+            return None 
+        
+        current_node  = self.head 
+        previous_node = None 
+
+        while current_node:
+            if current_node.value == value : 
+                if previous_node:
+                    previous_node.next = current_node.next 
+                else:
+                    self.head = current_node.next 
+                return 
+            
+            previous_node = current_node 
+            current_node = current_node.next 
+
 if __name__ == "__main__":
 
     ll = LinkedList() 
     ll.append(5)
     ll.prepend(8)
+   
+
     ll.append(6)
     ll.append(2)
     ll.show()
+    ll.delete(5)
+    ll.show()
+    
+
     
 
