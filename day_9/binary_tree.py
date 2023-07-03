bc6e9281b608f3ec69b55d24995db7dddf459d59
Adeleke Bright <bigjaratech@gmail.com>
2023-07-03 20:38:53 +0100

    Add Implementation for Binary Tree

--- day_9/binary_tree.py
+++ day_9/binary_tree.py
@@ -8,11 +8,11 @@ For a binary tree, each node can have a maximum of two children.
 
 
 '''
-class Node :
-    def __init__(self , value , left=None , right=None):
-        self.value = value 
-        self.left = left 
-        self.right = right 
+class BinaryNode :
+    def __init__(self ,data):
+        self.data = data 
+        self.left = None 
+        self.right = None 
     
 
 
@@ -20,5 +20,46 @@ class BinaryTree:
     def __init__(self) -> None:
         self.root = None 
     
-    def add_node(self,data):
-        return self.root
+    def insert(self,data):
+        if self.root is None :
+            self.root = BinaryNode(data)
+        else:
+            self.add_node(self.root , data) 
+    
+    def add_node(self , node , data):
+        if data < node.data :
+            if node.left is None :
+                node.left = BinaryNode(data)
+            else:
+                self.add_node(node.left , data)
+        else:
+            if node.right is None :
+                node.right = BinaryNode(data)
+            else:
+                self.add_node(node.right , data)
+    
+    def run(self):
+        self.traverse(self.root)
+    
+    def traverse(self , node):
+        if node is not None:
+            self.traverse(node.left)
+            print(node.data) 
+            self.traverse(node.right) 
+    
+if __name__ == "__main__":
+    
+   # Create the binary tree
+    tree = BinaryTree()
+
+    # Insert nodes into the binary tree
+    tree.insert(5)
+    tree.insert(3)
+    tree.insert(8)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(7)
+    tree.insert(9)
+
+    # Traverse the binary tree in inorder
+    tree.run()
