7cc8a203418cd6d8366e62c98c3b046546ebf3cc
Adeleke Bright <bigjaratech@gmail.com>
2023-07-22 23:34:32 +0100

    Add List Base Implementation for Keystroke comparison

--- day_15/keystrokes.py
+++ day_15/keystrokes.py
@@ -33,9 +33,25 @@ def compare_keystrokes(s , t):
 '''
 Note: The solution can be re-implemented using an array instead of a stack . 
 '''
+
+def apply_backspace_list(word):
+    stack = []
+    for char in word :
+        if char == "#":
+            stack.pop() 
+        else:
+            stack.append(char) 
+    
+    return "".join(stack) 
+
+def compare_keystrokes_list(s , t):
+    return apply_backspace_list(s) == apply_backspace_list(t)
+
 if __name__ == "__main__":
     s = "cof#dim#g"
     t = "codig"
 
     test = compare_keystrokes(s , t) 
+    print(test)
+    test = compare_keystrokes_list(s , t) 
     print(test)
