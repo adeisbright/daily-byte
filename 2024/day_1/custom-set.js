b28043ce3dd4113477d32566d582499356ecc044
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-01-09 12:13:55 +0100

    Add Custom Set

---
+++ 2024/day_1/custom-set.js
@@ -0,0 +1,55 @@
+/**
+ * CustomSet
+ * Properties
+ * ============
+ * setType - To hold the type of values stored in the set 
+ * setSize - To hold the size of the set 
+ * set - An instance property that refers to the data within our set 
+ * Methods
+ * ======== 
+ * add(value) - Add an item to the set 
+ * show() -  Display the items we have within our set 
+ * has(value) -> Returns true if the value is in our custom set 
+ */
+
+class CustomSet {
+    setType = null 
+    setSize = 0
+    constructor(){
+        this.set = {}
+    }
+    add(value){
+        if (this.setSize && this.setType){
+            if(typeof value !== this.setType){
+                throw new Error(`BadTypeError: ${value} should be of type ${this.setType}`)
+            }
+        }
+        if(!this.has(value)){
+            this.set[value] = value
+            this.setSize += 1
+        }
+        if(!this.setType){
+            this.setType = typeof value 
+        }
+        return this.set
+    }
+    has(value){
+        const hasValue = this.set[value] ? true  : false 
+        return  hasValue
+    }
+    show(){
+        for (let key of Object.keys(this.set)){
+            if(this.set.hasOwnProperty(key)){
+                console.log(key)
+            }
+        }
+    }
+}
+
+const mySet = new CustomSet() 
+mySet.add(4) 
+mySet.add(3)
+mySet.add(5)
+mySet.add(5)
+mySet.add(5)
+mySet.show()
